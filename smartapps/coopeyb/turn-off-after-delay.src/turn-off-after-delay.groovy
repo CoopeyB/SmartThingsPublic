c2e30c80867172da8f9aebd61f3b5dfe17106bb5
Cooper Bills <cbills07@gmail.com>
2017-06-22 00:05:04 -0400

    Add "Turn Off After Delay" App

---
+++ smartapps/coopeyb/turn-off-after-delay.src/turn-off-after-delay.groovy
@@ -0,0 +1,83 @@
+/**
+ *  Turn Off After Delay
+ *
+ *  Copyright 2017 COOPER BILLS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Turn Off After Delay",
+    namespace: "CoopeyB",
+    author: "Cooper Bills",
+    description: "Turn off a light after some time after a given contact sensor closes.",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("Turn off when this is closed") {
+        input "thedoor", "capability.contactSensor", required: true, title: "Sensor"
+    }
+    section("After seconds") {
+        input "thedelay", "number", required: true, title: "Dealy"
+    }
+    section("Turn off this light") {
+        input "theswitch", "capability.switch", required: true, title: "Light"
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+    subscribe(thedoor, "contact.closed", closeDetectedHandler)
+}
+
+def closeDetectedHandler(evt) {
+    log.debug "closeDetectedHandler called: $evt"
+    runIn(thedelay, checkDoor)
+}
+
+def checkDoor() {
+    log.debug "In checkDoor scheduled method"
+
+    // get the current state object
+    def doorState = thedoor.currentState("contact")
+
+    if (doorState.value == "closed") {
+        // get the time elapsed between now and when the sensor reported closed
+        def elapsed = now() - doorState.date.time
+
+        // elapsed time is in milliseconds, so the threshold must be converted to milliseconds too
+        def threshold = 1000 * (thedelay - 1)
+
+        if (elapsed >= threshold) {
+            log.debug "Door has stayed closed long enough since last check ($elapsed ms):  turning switch off"
+            theswitch.off()
+        } else {
+            log.debug "Door has not stayed closed long enough since last check ($elapsed ms):  doing nothing"
+        }
+    } else {
+            log.debug "Door is open, do nothing and wait for closed"
+    }
+}
